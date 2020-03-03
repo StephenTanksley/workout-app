0c0c4a3f337f32e7ab21bfecefb1ffb2ac600584
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-03-03 14:24:31 -0600

    Added a session table. Sessions will pull in data from other tables and collect it.

--- data/migrations/20200302161946_session.js
+++ data/migrations/20200302161946_session.js
@@ -1,17 +1,19 @@
 exports.up = async function(knex) {
-    table.increments('id').primary()
-    table.integer('workout_id').notNullable()
-        .references('id')
-        .inTable('workouts')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-    table.integer('user_id').notNullable()
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
+    await knex.schema.createTable('session', (table) => {
+        table.increments('id').primary()
+        table.integer('workout_id').notNullable()
+            .references('id')
+            .inTable('workouts')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+        table.integer('user_id').notNullable()
+            .references('id')
+            .inTable('users')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
 };
 
 exports.down = async function(knex) {
-  
+    await knex.schema.dropTableIfExists('session')
 };
